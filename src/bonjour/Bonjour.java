52458dc86b07bbe4a86d447ef789cc2c93d2078d
tinhinane <tinhinane.ait-messaoud@univ-tlse3.fr>
2023-04-20 17:54:50 +0200

    exigence 1

--- src/bonjour/Bonjour.java
+++ src/bonjour/Bonjour.java
@@ -1,5 +1,10 @@
 package bonjour;
 
 public class Bonjour {
-
+	
+	public String welcome(String nom) {
+		StringBuilder salutation = new StringBuilder();
+		salutation.append("Hello, ");
+		return "Hello, "+nom ;
+	}
 }
--- test/bonjour/BonjourTest.java
+++ test/bonjour/BonjourTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BonjourTest {
+	private Bonjour b;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		b=new Bonjour();
 	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void bonjour_bob() {
+		assertEquals("Hello, bob",b.welcome("bob"));
+		assertEquals("Hello, Bob",b.welcome("Bob"));
 	}
 
 }
